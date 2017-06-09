d762f78c706d591ab8343ad29b834e3926fd26d5
Matan Uberstein <matan@uberstein.co.za>
2017-06-09 14:16:14 +0200

    Added support for Adobe AIR (#3)
    * Added support for Adobe AIR
    
    * Added air3 def

--- src/tink/testrunner/Reporter.hx
+++ src/tink/testrunner/Reporter.hx
@@ -168,6 +168,8 @@ class BasicReporter implements Reporter {
 	function println(v:String)
 		#if travix
 			travix.Logger.println(v);
+		#elseif (air || air3)
+			flash.Lib.trace(v);
 		#elseif (sys || nodejs)
 			Sys.println(v);
 		#else
