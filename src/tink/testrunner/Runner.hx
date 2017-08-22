377d90afeebbba8752d971127382c446a884d132
Matan Uberstein <matan.uberstein@kidsindustries.com>
2017-08-22 18:27:22 +0200

    Added phantomjs print and exit support

--- src/tink/testrunner/Runner.hx
+++ src/tink/testrunner/Runner.hx
@@ -16,6 +16,7 @@ class Runner {
 		#if travix travix.Logger.exit
 		#elseif (air || air3) untyped __global__["flash.desktop.NativeApplication"].nativeApplication.exit
 		#elseif (sys || nodejs) Sys.exit
+		#elseif (phantomjs) untyped __js__('phantom').exit
 		#else throw "not supported";
 		#end (result.summary().failures.length);
 	
